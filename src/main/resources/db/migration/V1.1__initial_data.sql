2471a8a4b32f8149c6e7dd2d024a540b0b3b5635
nabillaindr <nabilla.febry@akarinti.tech>
2023-02-26 18:46:04 +0700

    add query insert into user

--- src/main/resources/db/migration/V1.1__initial_data.sql
+++ src/main/resources/db/migration/V1.1__initial_data.sql
@@ -3,4 +3,11 @@ VALUES
 ('admin', 'admin aplikasi'),
 ('dinas perikanan', 'dinas perikanan Kepulauan Aru'),
 ('nelayan', 'nelayan di Kepulauan Aru'),
-('pembeli', 'pembeli ikan')
+('pembeli', 'pembeli ikan');
+
+INSERT INTO user(id_user, id_role, username, password, status)
+VALUES
+('17264917', 1, 'admin1', 'password', 'ACTIVE'),
+('16356294', 1, 'admin2', 'password', 'ACTIVE'),
+('25374917', 2, 'gov1', 'password', 'ACTIVE'),
+('24516491', 2, 'gov2', 'password', 'ACTIVE');
