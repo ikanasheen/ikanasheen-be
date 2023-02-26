7fc1e119e25284354ba84b2663be3e3b56929459
nabillaindr <nabilla.febry@akarinti.tech>
2023-02-26 21:37:03 +0700

    add query insert into admin & dinas perikanan

--- src/main/resources/db/migration/V1.1__initial_data.sql
+++ src/main/resources/db/migration/V1.1__initial_data.sql
@@ -14,7 +14,7 @@ VALUES
 
 INSERT INTO admin(id_admin, id_user, no_telepon, email)
 VALUES
-('17264917', 1, 'admin1', 'password', 'ACTIVE'),
-('16356294', 1, 'admin2', 'password', 'ACTIVE'),
-('25374917', 2, 'gov1', 'password', 'ACTIVE'),
-('24516491', 2, 'gov2', 'password', 'ACTIVE');
+('17264917230225', '17264917', '081234567890', 'admin1@asheen.com'),
+('16356294230225', '16356294', '081234567890', 'admin2@asheen.com'),
+('25374917230225', '25374917', '081098765432', 'gov1@gmail.com'),
+('24516491230225', '24516491', '081098765432', 'gov2@gmail.com');
