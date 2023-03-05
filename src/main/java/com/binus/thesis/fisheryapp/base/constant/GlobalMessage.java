088013bcddf6a6121f9c5dc3fcdc1163c408bdbd
nabillaindr <nabilla.febry@akarinti.tech>
2023-03-05 22:43:38 +0700

    refactor global message

--- src/main/java/com/binus/thesis/fisheryapp/base/constant/GlobalMessage.java
+++ src/main/java/com/binus/thesis/fisheryapp/base/constant/GlobalMessage.java
@@ -18,12 +18,12 @@ public @interface GlobalMessage {
                 "#Variable4",
                 "#Variable5");
 
-        public static final String FIELD_REQUIRED = "field " + paramVariable.get(0) + " tidak boleh kosong";
-        public static final String USER_NOT_REGISTERED = "user belum terdaftar";
-        public static final String INVALID_PASSWORD = "password salah";
-        public static final String USER_ALREADY_EXISTS = "username " + paramVariable.get(0) + " sudah digunakan";
-        public static final String DATA_ALREADY_EXISTS = "data " + paramVariable.get(0) + " sudah ada";
-        public static final String DATA_NOT_FOUND = "data " + paramVariable.get(0) + " dengan ID " + paramVariable.get(1) + " tidak ditemukan";
+        public static final String FIELD_REQUIRED = "Filed " + paramVariable.get(0) + " tidak boleh kosong!";
+        public static final String USER_NOT_REGISTERED = "User belum terdaftar!";
+        public static final String INVALID_PASSWORD = "Password yang Anda masukkan salah!";
+        public static final String USER_ALREADY_EXISTS = "Username " + paramVariable.get(0) + " sudah digunakan!";
+        public static final String DATA_ALREADY_EXISTS = "Data " + paramVariable.get(0) + " sudah ada!";
+        public static final String DATA_NOT_FOUND = "Data " + paramVariable.get(0) + " dengan ID " + paramVariable.get(1) + " tidak ditemukan!";
     }
 
     class Resp {
@@ -37,10 +37,10 @@ public @interface GlobalMessage {
                 "#Variable4",
                 "#Variable5");
 
-        public static final String SUCCESS_CREATE_ACCOUNT = "akun berhasil dibuat";
-        public static final String SUCESS_GET_DATA = "berhasil mengambil data";
-        public static final String SUCCESS_CREATE_DATA = "data berhasil dibuat";
-        public static final String SUCCESS_UPDATE_DATA = "data berhasil diubah";
-        public static final String SUCCESS_DELETE_DATA = "data berhasil dihapus";
+        public static final String SUCCESS_CREATE_ACCOUNT = "Akun berhasil dibuat";
+        public static final String SUCESS_GET_DATA = "Berhasil mengambil data";
+        public static final String SUCCESS_CREATE_DATA = "Data berhasil dibuat";
+        public static final String SUCCESS_UPDATE_DATA = "Data berhasil diubah";
+        public static final String SUCCESS_DELETE_DATA = "Data berhasil dihapus";
     }
 }
